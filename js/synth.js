f308cee96fa6bd6ad60ebe14f93ec0dcd4bbc690
Benoit <benoit@hopsandfork.com>
2015-01-06 16:24:02 +0100

    Clean synth code, add more oscillator properties

--- js/synth.js
+++ js/synth.js
@@ -1,53 +1,24 @@
 var audioCtx = new AudioContext();
 var oscillator = audioCtx.createOscillator();
 var gainNode = audioCtx.createGain();
+var initialFreq = 800;
+var initialVol = 0.1;
+var dotTime = 200
 gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+gainNode.gain.value = initialVol;
 oscillator.connect(gainNode);
+oscillator.type = 'sine';
+oscillator.frequency.value = initialFreq; // value in hertz
+
 gainNode.connect(audioCtx.destination);
 oscillator.start();
 
 function trigger() {
-  console.log("trigger");
-  gainNode.gain.setValueAtTime(0.5, audioCtx.currentTime);
-  gainNode.gain.setTargetAtTime(0.0, audioCtx.currentTime + 0.01, 0.5);
+  gainNode.gain.setValueAtTime(0.8, audioCtx.currentTime);
+  gainNode.gain.setTargetAtTime(0.0, audioCtx.currentTime + 0.0, 0.1);
 }
 
 var mute = document.querySelector('.mute');
 mute.ontouchstart = trigger;
 mute.onclick= trigger;
 
-/*
-var initialFreq = 800;
-var initialVol = 0.1;
-var dotTime = 200
-oscillator.type = 'sine';
-oscillator.frequency.value = initialFreq; // value in hertz
-oscillator.start();
-
-gainNode.gain.value = initialVol;
-
-var morseStatut = morseEmmit(dotTime);
-
-function morseEmmit(dotTime){
-  if ( morseStatut != null )
-  {
-    gainNode.connect(audioCtx.destination);
-    window.setTimeout("gainNode.disconnect(audioCtx.destination)", 1200);
-  }
-  else
-  {
-    window.setTimeout("morseEmmit(dotTime);",100);
-  }
-}
-
-function computeVariable(morseStatut) {
-  // arithmetic operations that use myVar [2]
-}
-
-
-// mute button
-var mute = document.querySelector('.mute');
-mute.onclick = function() {
-  morseEmmit(300);
-};
-*/
