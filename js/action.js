5dae300c7bb0bf300f2b78eeeab9b65f3f797ee0
Benoit <benoit@hopsandfork.com>
2015-01-06 11:06:28 +0100

    Fix synth button

--- js/action.js
+++ js/action.js
@@ -1,29 +1,27 @@
 var morseField = document.getElementById('morseField');
 var morseConverter = new MorseCode();
 
-
 document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    morseConverted = morseConverter.translate(morseField.value.toLowerCase());
-    document.getElementById('morse_render').innerHTML = morseConverted;
+  e.preventDefault();
+  morseConverted = morseConverter.translate(morseField.value.toLowerCase());
+  document.getElementById('morse_render').innerHTML = morseConverted;
 
-    var morseConvertedA = morseConverted.split('');
-    var morseLength = morseConvertedA.length;
+  var morseConvertedA = morseConverted.split('');
+  var morseLength = morseConvertedA.length;
 
-    for (var morseChar = 0; morseChar < morseLength; morseChar++) {
-            console.log(morseConvertedA[morseChar]);
-            if (morseConvertedA[morseChar] == "_") {
-                morseEmmit(500);
-                console.log("500");
-            } else if (morseConvertedA[morseChar] == ".") {
-                morseEmmit(200);
-                console.log("500");
-            } else if (morseConvertedA[morseChar] == " ") {
-                console.log("space");
-                window.setTimeout(800);
-            } else {
-                  console.log(flag);
-        }
+  for (var morseChar = 0; morseChar < morseLength; morseChar++) {
+    console.log(morseConvertedA[morseChar]);
+    if (morseConvertedA[morseChar] == "_") {
+      morseEmmit(500);
+      console.log("500");
+    } else if (morseConvertedA[morseChar] == ".") {
+      morseEmmit(200);
+      console.log("500");
+    } else if (morseConvertedA[morseChar] == " ") {
+      console.log("space");
+      window.setTimeout(800);
+    } else {
+      console.log(flag);
     }
-
+  }
 });
--- js/synth.js
+++ js/synth.js
@@ -1,36 +1,53 @@
-// create web audio api context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-// create Oscillator and gain node
+var audioCtx = new AudioContext();
 var oscillator = audioCtx.createOscillator();
 var gainNode = audioCtx.createGain();
-// connect oscillator to gain node to speakers
+gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
 oscillator.connect(gainNode);
-// oscillator parameters
+gainNode.connect(audioCtx.destination);
+oscillator.start();
+
+function trigger() {
+  console.log("trigger");
+  gainNode.gain.setValueAtTime(0.5, audioCtx.currentTime);
+  gainNode.gain.setTargetAtTime(0.0, audioCtx.currentTime + 0.01, 0.5);
+}
+
+var mute = document.querySelector('.mute');
+mute.ontouchstart = trigger;
+mute.onclick= trigger;
+
+/*
 var initialFreq = 800;
 var initialVol = 0.1;
 var dotTime = 200
-// set options for the oscillator
 oscillator.type = 'sine';
 oscillator.frequency.value = initialFreq; // value in hertz
 oscillator.start();
 
 gainNode.gain.value = initialVol;
 
-var flag = true;
+var morseStatut = morseEmmit(dotTime);
 
-
-function morseEmmit(dotTime) {
-	flag = false
-	console.log(flag);
+function morseEmmit(dotTime){
+  if ( morseStatut != null )
+  {
     gainNode.connect(audioCtx.destination);
     window.setTimeout("gainNode.disconnect(audioCtx.destination)", 1200);
-    window.setTimeout("var flag = true", 1400);
-    window.setTimeout("console.log(flag)", 1400);
+  }
+  else
+  {
+    window.setTimeout("morseEmmit(dotTime);",100);
+  }
 }
+
+function computeVariable(morseStatut) {
+  // arithmetic operations that use myVar [2]
+}
+
 
 // mute button
 var mute = document.querySelector('.mute');
 mute.onclick = function() {
-	morseEmmit(300);
+  morseEmmit(300);
 };
-
+*/
